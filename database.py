56b9009e81808f5a67c34ee3f3c0102d22c08590
Kooten111 <bostrom91@gmail.com>
2025-11-29 18:33:28 +0100

    Enable SQLite WAL mode to fix UI hangs during ingest

--- database.py
+++ database.py
@@ -7,6 +7,8 @@ def get_db_connection():
     """Create a database connection."""
     conn = sqlite3.connect(DB_FILE)
     conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute("PRAGMA journal_mode = WAL")
+    conn.execute("PRAGMA synchronous = NORMAL")
     conn.row_factory = sqlite3.Row
     return conn
 
